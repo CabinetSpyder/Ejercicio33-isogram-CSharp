f280f8f872e4ae008f3a6d1da7287435c9563b22
¨CabinetSpyder¨ <andres.artaloytia@gmail.com>
2025-05-07 11:07:18 +0200

    Primera iteracion de la solucion

--- isogram/Isogram.cs
+++ isogram/Isogram.cs
@@ -2,6 +2,33 @@ public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        /*
+        Idea, lo mismo que el anterior, ir metiendo todas las letras en una list e ir comprobando 
+        si hay alguna repetida. Si lo hay, devuelves false
+        */
+        if(string.IsNullOrWhiteSpace(word))
+        {
+            return true;
+        }
+
+        List<char> auxStr = [];
+        word = word.ToLower();
+
+        foreach(char ch in word)
+        {
+            if(char.IsLetter(ch))
+            {
+                if(auxStr.Contains(ch))
+                {
+                    return false;
+                }else
+                {
+                    auxStr.Add(ch);
+                }
+            }
+        }
+
+        return true;
+
     }
 }
